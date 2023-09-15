446c59fc9dd538131f0cda72d2d72bacfe632c1c
Thomas Seely <tom.seely@icloud.com>
2023-09-15 12:44:15 -0400

    made both operators abstract and made an example OR block using AND and NOT

--- src/Operator2I.java
+++ src/Operator2I.java
@@ -1,5 +1,5 @@
 //takes two inputs rather than default 1
-public class Operator2I extends Operator{
+public abstract class Operator2I extends Operator{
     protected Operator previous2;
 
     public Operator getPrev2(){
@@ -9,13 +9,5 @@ public class Operator2I extends Operator{
         previous2 = n;
     }
     //This and Operator shouldn't be existing objects in the first place so this shouldn't run
-    public boolean getOutput(){         //should run when there is no more previous
-        if (previous1 == null){         //if previous1 is null then 2 is also null by default
-            return source;              //and thus has no inputs, therefore taking source as output
-        }
-        else if (previous2 == null){    //previous1 only channel so just takes and outputs that
-            return previous1.getOutput();
-        }
-        return false;                   //default operation false if nothing outputs to it
-    }
+    public abstract boolean getOutput();
 }
