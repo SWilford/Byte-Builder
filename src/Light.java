d855e9279dfff7facf28370e705ad4c27b2460c8
Thomas Seely <tom.seely@icloud.com>
2023-09-26 12:54:14 -0400

    light operator

---
+++ src/Light.java
@@ -0,0 +1,9 @@
+public class Light extends Operator{
+
+    public Light(Operator n){
+        super(n);
+    }
+    public boolean getOutput() {
+        return previous1.getOutput(); //merely returns the signal it is given, used to show the signal in the game, it can also be passed to later operators if need be
+    }
+}
